8f519b41d5302e7943a90b19e05e7547a5b59207
Adi Jain <adi1510jain@gmail.com>
2025-10-20 02:26:24 +0530

    Home page started

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,50 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-    return <div>Home Page</div>;
+    return (
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2 className="text-primary-100">
+                        Interview Ready with AI-powered Practice & Feedback
+                    </h2>
+                    <p className="text-lg">
+                        Practice on real interview questions and get instant
+                        feedback.
+                    </p>
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Get Started</Link>
+                    </Button>
+                </div>
+                <Image src="/robot.png" alt="robo" width={400} height={400} />
+            </section>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+
+                    <p>You haven&apos;t taken any interviews yet.</p>
+                </div>
+            </section>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+
+                    <p>There are no interviews available yet.</p>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Page;
