40a7ad8d2eab537298e8dd77dce9291d7e6c328f
Adi Jain <adi1510jain@gmail.com>
2025-10-26 22:58:56 +0530

    Minor progress

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,4 +1,6 @@
+import { getRandomInterviewCover } from "@/lib/utils";
 import dayjs from "dayjs";
+import Image from "next/image";
 
 const InterviewCard = ({
     interviewId,
@@ -13,7 +15,36 @@ const InterviewCard = ({
     const formattedDate = dayjs(
         feedback?.createdAt || createdAt || Date.now()
     ).format("MMM D, YYYY");
-    return <div>InterviewCard</div>;
+    return (
+        <div className="card-border w-[360px] max-sm:w-full min-h-96">
+            <div className="card-interview">
+                <div>
+                    <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+                        <p className="badge-text">{normalizedType}</p>
+                    </div>
+                    <Image
+                        src={getRandomInterviewCover()}
+                        alt="coverImage"
+                        width={90}
+                        height={90}
+                        className="rounded-full object-fit size-[90px]"
+                    />
+                    <h3 className="mt-5 capitalize">{role} Interview</h3>
+                    <div className="flex flex-row gap-5 mt-3">
+                        <div className="flex flex-row gap-2">
+                            <Image
+                                src="/calendar.svg"
+                                alt="calendar"
+                                width={22}
+                                height={22}
+                            />
+                            <p>{formattedDate}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default InterviewCard;
